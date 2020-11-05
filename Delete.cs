065fdb697d8f616c731f1c8eb4911b4dbccda93a
Савицкий Даня <savitskiydanulo@gmail.com>
2020-11-06 00:12:42 +0200

    Решил 1-ую кату. Во второй кате возникли вопросы: Я помню что можно разбить строку по пробелам и присвоить ее массиву. (надо почитать)

---
+++ Delete.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Delete
+    {
+        public int TestValue { get; set; } = 5;
+
+        public void DisplayValue()
+        {
+            Console.WriteLine(TestValue);
+        }
+
+        public static void DisplayValueStatic()
+        {
+            Delete delete = new Delete();
+            Console.WriteLine(delete.TestValue);
+
+            int i = Int32.Parse(Console.ReadLine());
+        }
+    }
+}
