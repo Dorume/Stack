0f5b64f451a4e277fccf7cc4916021f5d8380112
Савицкий Даня <savitskiydanulo@gmail.com>
2020-11-07 12:46:07 +0200

    Почистил код

--- Delete.cs
+++ Delete.cs
@@ -8,19 +8,5 @@ namespace Stack
 {
     class Delete
     {
-        public int TestValue { get; set; } = 5;
-
-        public void DisplayValue()
-        {
-            Console.WriteLine(TestValue);
-        }
-
-        public static void DisplayValueStatic()
-        {
-            Delete delete = new Delete();
-            Console.WriteLine(delete.TestValue);
-
-            int i = Int32.Parse(Console.ReadLine());
-        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,36 +10,7 @@ namespace Stack
     {
         static void Main(string[] args)
         {
-            string words;
-            words = Console.ReadLine();
-            Order(words);
         }
 
-        public static string Order(string words)
-        {
-            string answer;
-
-            return answer;
-        }
-
-        public static int CountBits(int input)
-        {
-            int powerOfTwo = 1, countOfBits = 0;
-            while (input > 0)
-            {
-                if (input >= powerOfTwo)
-                {
-                    powerOfTwo *= 2;
-                }
-                else
-                {
-                    countOfBits++;
-                    input -= powerOfTwo/2;
-                    powerOfTwo = 1;
-                }
-
-            }
-            return countOfBits;
-        }
     }
 }
